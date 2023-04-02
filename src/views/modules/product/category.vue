0d413ea29d5f49b1f217ae5c98d77b90d856b59c
chaobk <1004945427@qq.com>
2023-04-02 22:13:31 +0800

    增加分类的 Append、Delete 按钮

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,21 @@
 <template>
-    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="menus" 
+    :props="defaultProps" 
+    :expand-on-click-node="false" 
+    show-checkbox 
+    node-key="catId">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+            <span>{{ node.label }}</span>
+            <span>
+                <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+                    Append
+                </el-button>
+                <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
+                    Delete
+                </el-button>
+            </span>
+        </span>
+    </el-tree>
 </template>
 
 <script>
@@ -24,17 +40,21 @@ export default {
     //监控 data 中的数据变化
     watch: {},
     methods: {
-        handleNodeClick(data) {
-            console.log(data);
-        },
         getMenus() {
             this.$http({
                 url: this.$http.adornUrl('/product/category/list/tree'),
                 method: 'get'
             }).then(({ data }) => {
-               console.log("成功获取到菜单数据...", data.data)
-               this.menus = data.data
+                console.log("成功获取到菜单数据...", data.data)
+                this.menus = data.data
             })
+        },
+        append(data) {
+            console.log("append", data);
+        },
+
+        remove(node, data) {
+            console.log("delete", node, data);
         }
     },
     //生命周期 - 创建完成（可以访问当前 this 实例）
